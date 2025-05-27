e2252d77d738f6d1e4c025eeca1ee9e6430cce02
Pedroo722 <pedrohenriquealexandre7@gmail.com>
2025-05-27 12:10:30 -0300

    Commit Inicial

---
+++ src/app/pages/ExamplePage.tsx
@@ -0,0 +1,12 @@
+import ExampleComponent from '../components/ExampleComponent';
+
+const ExamplePage = () => {
+	return (
+		<main>
+			<h1>Example Page</h1>
+			<ExampleComponent />
+		</main>
+	);
+};
+
+export default ExamplePage;
